d5fe9f1cdb660e45ac2e594b8ed581bb0c90d621
ducle <duc.leduongg@gmail.com>
2023-08-22 22:23:06 +0700

    implement UI

---
+++ app/create-order/page.tsx
@@ -0,0 +1,13 @@
+import CreateOrderForm from "@/components/CreateOrderForm/CreateOrderForm";
+import Layout from "@/components/Layout/Layout";
+import React from "react";
+
+const CreateOrder: React.FC = () => {
+  return (
+    <Layout>
+      <CreateOrderForm />
+    </Layout>
+  );
+};
+
+export default CreateOrder;
