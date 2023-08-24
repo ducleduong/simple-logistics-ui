7637eeb659a08e8d5c91d8145bbf72a2ff34b586
ducle <duc.leduongg@gmail.com>
2023-08-24 15:27:40 +0700

    redirect login when unauthorized

--- api/config.ts
+++ api/config.ts
@@ -1,18 +1,26 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL,
-    headers: {
-        'Content-Type': 'application/json'
-    }
-})
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
 
 api.interceptors.request.use((config) => {
-    if(!config.url?.includes('login') || !config.url?.includes('register')) {
-        const token = window.localStorage.getItem('access_token');
-        config.headers['Authorization'] = `Bearer ${token}`;
+  if (!config.url?.includes("login") || !config.url?.includes("register")) {
+    const token = window.localStorage.getItem("access_token");
+    config.headers["Authorization"] = `Bearer ${token}`;
+  }
+  return config;
+});
+
+api.interceptors.response.use((config) => {
+  return config;
+}, (err) => {
+    if (err.response.data.statusCode === 401) {
+        window.location.href = '/login'
     }
-    return config;
-})
+});
 
 export default api;
